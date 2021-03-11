950e3d1c1de99525714675c4adaafc6434d288e7
dqy <752408303@qq.com>
2021-03-11 17:37:52 +0800

    + add template

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ program.version('1.0.0', '-v, --version')
     .command('init <name>')
     .action((name) => {
         console.log('clone template ...');
-        process1.exec('git clone (git 下载地址) ' + name, function(error, stdout, stderr) {
+        process1.exec('git clone git@github.com:Excalibur0w0/burry-template-vue.git' + name, function(error, stdout, stderr) {
             if (error !== null) {
                 console.log('exec error: ' + error);
                 return;
